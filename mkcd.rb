3c8a6521de5f1de588bcd587b322342acaceff48
TIRUPATS <bag-poodle07@icloud.com>
2024-05-30 12:29:39 -0700

    Add mkcd path

--- mkcd.rb
+++ mkcd.rb
@@ -1,7 +1,7 @@
 class Mkcd < Formula
     desc "Command-line utility to create directories and change into them"
     homepage "https://github.com/tirupats/mkcd"
-    url "https://github.com/tirupats/mkcd/archive/v1.0.0.tar.gz"
+    url "https://github.com/tirupats/mkcd/blob/master/archive/mkcd-1.0.0.tar.gz"
     sha256 "dfc28630d125cc67f69597af87f68b5350c13b3e4d1abd61a4a7ef6a9806c37a"
     version "1.0.0"
   
