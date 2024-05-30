c1136f40524bfecea4682bd27242d8d3a2b2a048
TIRUPATS <bag-poodle07@icloud.com>
2024-05-30 13:06:43 -0700

    Update SHA in mkcd.rb formula

--- mkcd.rb
+++ mkcd.rb
@@ -2,7 +2,7 @@ class Mkcd < Formula
     desc "Command-line utility to create directories and change into them"
     homepage "https://github.com/tirupats/mkcd"
     url "https://github.com/tirupats/mkcd/blob/master/archive/mkcd-1.0.0.tar.gz"
-    sha256 "44569979b893e6478515c904f15ce3e746f1fec3b83a61187e4db62cb4cf33b4"
+    sha256 "be98e612dfebe54e3a3e949f96e79976eaf1a682cc1da355329f06ee07b8a4d1"
     version "1.0.0"
   
     def install
