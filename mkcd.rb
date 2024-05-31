a0d84a867ba985c58dc25f171a46aa95bab46f19
TIRUPATS <bag-poodle07@icloud.com>
2024-05-30 22:53:40 -0700

    Updated mkcd formula with new release

--- mkcd.rb
+++ mkcd.rb
@@ -1,8 +1,8 @@
 class Mkcd < Formula
     desc "Command-line utility to create directories and change into them"
     homepage "https://github.com/tirupats/mkcd"
-    url "https://github.com/tirupats/mkcd/blob/master/archive/mkcdv1.tar.gz"
-    sha256 "f9cd2bbdfdbbd45638047e2f61ceb296c3123254805d511a8c9ff5691598a7ed"
+    url "https://github.com/tirupats/mkcd/releases/download/v1.0.0/mkcd-1.0.0.tar.gz"
+    sha256 "cd3d2a06924536592f789c3ab956175050210e5234d3f4fba01962241db215ba"
     version "1.0.0"
   
     def install
